9c99a4616dc02621ac410fb1915567ee37035b82
mig07 <miguelfluis7@gmail.com>
2022-03-28 17:30:59 +0100

    Updated workflow table

--- src/page/workflowList.js
+++ src/page/workflowList.js
@@ -12,43 +12,48 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-    createData('MyFirstPipeline', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('MyFirstPipeline', "This is my first pipeline", new Date().toDateString(), "-", "Finished"),
+    createData('ExamplePipeline', "This is an example pipeline", new Date().toDateString(), "-", "Executing"),
+    createData('TrimmomaticPipeline', "A trimmomatic pipeline", new Date().toDateString(), "-", "Executing"),
 ];
+
+const tableStyle={
+  TableCell: {
+    align: "left"
+  }
+}
 
 export default function WorkflowList() {
     return (
+      <div style={tableStyle}>
         <TableContainer sx={{marginTop:5}} component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Creation date</TableCell>
-            <TableCell align="right">Finish date</TableCell>
-            <TableCell align="right">State</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Creation date</TableCell>
+            <TableCell>Finish date</TableCell>
+            <TableCell>State</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.carbs}</TableCell>
+              <TableCell>{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
+  </div>
   ); 
 }
