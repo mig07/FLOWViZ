67ec00ba9564472200008603061b16b58ee838a6
mig07 <miguelfluis7@gmail.com>
2022-04-12 13:06:59 +0100

    Client - Started Not Found page

---
+++ client/src/page/notFound.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Typography, useMediaQuery } from '@material-ui/core';
+
+export default function NotFound() {
+    return (
+        <Typography variant="h1">Page Not Found!</Typography>
+    );    
+}
