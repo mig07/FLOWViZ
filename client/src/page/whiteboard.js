f8953f2c3102b9fed27ea8438acd3f005295dc19
mig07 <miguelfluis7@gmail.com>
2022-05-08 21:57:11 +0100

    Client: config for Workflow component; code formatting

--- client/src/page/whiteboard.js
+++ client/src/page/whiteboard.js
@@ -1,25 +1,26 @@
-import * as React from 'react';
-import {useState, useEffect} from 'react';
-import ToolDrawer from '../component/whiteboard/toolDrawer';
-import Workflow from '../component/workflow/workflow';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import ToolDrawer from "../component/whiteboard/toolDrawer";
+import ToolSetupDrawer from "../component/whiteboard/toolSetupDrawer";
+import Workflow from "../component/workflow/workflow";
 
 export default function Whiteboard(props) {
+  const config = props.config;
+  const uri = `${config.appProtocol}://${config.address}:${config.port}/library`;
 
-    const uri = `${props.config.appProtocol}://${props.config.address}:${props.config.port}/library`
+  // Libraries and APIs state hook
+  const [list, setList] = useState([]);
 
-    // Libraries and APIs state hook
-    const [list, setList] = useState([])
-        
-    useEffect(()=>{
-        fetch(uri)
-          .then(response => response.json())
-          .then(setList);
-    }, []);
+  useEffect(() => {
+    fetch(uri)
+      .then((response) => response.json())
+      .then(setList);
+  }, []);
 
-    return (
-        <>
-            <ToolDrawer tools={list}/>
-            <Workflow/> 
-        </>
-    ); 
+  return (
+    <>
+      <ToolDrawer tools={list} />
+      <Workflow config={config} />
+    </>
+  );
 }
