fac41fedc0195512b9792e34af34c446d6876845
mig07 <miguelfluis7@gmail.com>
2022-05-10 23:06:46 +0100

    Client: Code formatting and small fixes

--- client/src/component/common/userForm.js
+++ client/src/component/common/userForm.js
@@ -1,141 +1,139 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Toolbar } from '@mui/material';
-import Theme from '../../config/theme';
-
-const theme = createTheme();
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Toolbar } from "@mui/material";
+import Theme from "../../config/theme";
 
 function LoginForm() {
-    return (
-        <>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                //autoFocus
-            />
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-            />
-        </>
-    )
+  return (
+    <>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        //autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+    </>
+  );
 }
 
 function RegisterForm() {
-    return (
-        <>
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Name"
-                name="name"
-                autoComplete="name"
-                //autoFocus
-            />
-            <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="surname"
-                label="Surname"
-                name="Surname"
-                autoComplete="Surname"
-            />
-            { LoginForm() }
-        </>
-    )
+  return (
+    <>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="name"
+        label="Name"
+        name="name"
+        autoComplete="name"
+        //autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="surname"
+        label="Surname"
+        name="Surname"
+        autoComplete="Surname"
+      />
+      {LoginForm()}
+    </>
+  );
 }
 
 export default function UserForm(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-          email: data.get('email'),
-          password: data.get('password'),
-        });
-      };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Toolbar />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: Theme.palette.secondary.main }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {props.operationName}
+        </Typography>
 
-    return (
-    <ThemeProvider theme={Theme}>
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Toolbar />
-        <Box
-            sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            }}
-        >
-            <Avatar sx={{ m: 1, bgcolor: Theme.palette.secondary.main }}>
-            <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-                {props.operationName}
-            </Typography>    
-
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            { (() => {
-                switch(props.operationName) {
-                    case "Register": return <RegisterForm/>
-                    case "Login": return <LoginForm/>
-                }
-                })()
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          {(() => {
+            switch (props.operationName) {
+              case "Register":
+                return <RegisterForm />;
+              case "Login":
+                return <LoginForm />;
             }
-            <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2, bgcolor: Theme.palette.primary.dark }}
-            >
-                {props.operationName}
-            </Button>
-            <Grid container>
-                <Grid item xs>
-                <Link href="#" variant="body2">
-                    Forgot password?
-                </Link>
-                </Grid>
-                <Grid item>
-                <Link href="#" variant="body2">
-                    {(() => {if (props.operationName === "Login") return "Don't have an account? Sign Up"})()}
-                </Link>
-                </Grid>
+          })()}
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, bgcolor: Theme.palette.primary.dark }}
+          >
+            {props.operationName}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
             </Grid>
-            </Box>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {(() => {
+                  if (props.operationName === "Login")
+                    return "Don't have an account? Sign Up";
+                })()}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        </Container>
-    </ThemeProvider>
-    );
+      </Box>
+    </Container>
+  );
 }
--- client/src/page/whiteboard.js
+++ client/src/page/whiteboard.js
@@ -1,15 +1,50 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useCallback, useRef, useEffect } from "react";
+import ReactFlow, {
+  Background,
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  updateEdge,
+  Controls,
+  MiniMap,
+} from "react-flow-renderer";
+import ToolNode from "../component/whiteboard/toolNode";
 import ToolDrawer from "../component/whiteboard/toolDrawer";
-import ToolSetupDrawer from "../component/whiteboard/toolSetupDrawer";
-import Workflow from "../component/workflow/workflow";
+import ToolSetupDialog from "../component/whiteboard/toolSetupDialog";
+
+let id = -1;
+const getId = () => `node${++id}`;
+
+const nodeWidth = 150;
+const nodeHeight = 40;
+
+const nodeTypes = { tool: ToolNode };
 
 export default function Whiteboard(props) {
   const config = props.config;
   const uri = `${config.appProtocol}://${config.address}:${config.port}/library`;
 
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
   // Libraries and APIs state hook
   const [list, setList] = useState([]);
+
+  // Tool setup dialog state hooks
+  const [openToolSetup, setOpenToolSetup] = React.useState(false);
+  const [toolSetupScroll, setToolSetupScroll] = React.useState("paper");
+
+  const onToolSetupButtonOpen = (scrollType) => () => {
+    setOpenToolSetup(true);
+    setToolSetupScroll(scrollType);
+  };
+
+  const onToolSetupButtonClose = () => {
+    setOpenToolSetup(false);
+  };
 
   useEffect(() => {
     fetch(uri)
@@ -17,10 +52,118 @@ export default function Whiteboard(props) {
       .then(setList);
   }, []);
 
+  // Set edges
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+  // Update edges
+  const onEdgeUpdate = (oldEdge, newConnection) =>
+    setEdges((els) => updateEdge(oldEdge, newConnection, els));
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onPaneClick = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      // If the event wasn't triggered by a left mouse button click, then return.
+      if (event.button !== 0) {
+        return;
+      }
+
+      // Register the click position
+      const position = reactFlowInstance.project({
+        x: event.clientX,
+        y: event.clientY,
+      });
+
+      // Create the new node
+      const newNode = {
+        id: getId(),
+        type: "default",
+        position,
+        style: { width: nodeWidth, height: nodeHeight },
+        data: { label: `Node ${id}` },
+      };
+
+      // Concat new node
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance]
+  );
+
+  const onDrop = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      const toolName = event.dataTransfer.getData("application/reactflow");
+
+      const position = reactFlowInstance.project({
+        x: event.clientX - reactFlowBounds.left,
+        y: event.clientY - reactFlowBounds.top,
+      });
+
+      // TODO - change for a requester class
+      const uri = `${config.appProtocol}://${config.address}:${config.port}/library/${toolName}`;
+      const request = await fetch(uri);
+      const response = await request.json();
+
+      const newNode = {
+        id: getId(),
+        type: "tool",
+        position: position,
+        data: {
+          label: toolName,
+          tool: response,
+          onToolSetupButtonOpen: onToolSetupButtonOpen,
+        },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance]
+  );
+
   return (
     <>
       <ToolDrawer tools={list} />
-      <Workflow config={config} />
+      <ToolSetupDialog
+        openToolSetup={openToolSetup}
+        toolSetupScroll={toolSetupScroll}
+        onToolSetupButtonClose={onToolSetupButtonClose}
+      />
+      <ReactFlowProvider>
+        <div
+          className="reactflow-wrapper"
+          ref={reactFlowWrapper}
+          style={{ height: "100vh", width: "100vw" }}
+        >
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            /* onPaneClick={onPaneClick} */
+            onDragOver={onDragOver}
+            onDrop={onDrop}
+            onEdgeUpdate={onEdgeUpdate}
+            deleteKeyCode={"Delete"}
+            nodeTypes={nodeTypes}
+            fitView
+          >
+            <MiniMap />
+            <Controls />
+            <Background variant="lines" color="#aaa" gap={12} />
+          </ReactFlow>
+        </div>
+      </ReactFlowProvider>
     </>
   );
 }
