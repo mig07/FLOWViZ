7a1c9e6fd09323e628d451cba527950d89077fda
mig07 <miguelfluis7@gmail.com>
2022-06-23 22:51:07 +0100

    Server: linted toolDbDataSource.js

--- server/src/datasource/toolDbDataSource.js
+++ server/src/datasource/toolDbDataSource.js
@@ -1,27 +1,25 @@
-const ToolContract = require('../schema/toolContract/ToolContract');
+const ToolContract = require("../schema/toolContract/ToolContract");
 
 module.exports = () => {
+  function getTools() {
+    return ToolContract.find({});
+  }
 
-    function getTools() {
-        return ToolContract.find({})            
-    }
+  function getTool(toolName) {
+    return ToolContract.findOne({ name: toolName });
+  }
 
-    function getTool(toolName) {
-        return ToolContract.findOne({ name: toolName })
-    }
+  function addTool(tool) {
+    return new ToolContract(tool).save();
+  }
 
-    function addTool(tool) {    
-        return new ToolContract(tool).save()
-            
-    }
+  function updateTool(toolName, updatedTool) {
+    return new ToolContract.replaceOne({ name: toolName }, updatedTool);
+  }
 
-    function updateTool(toolName, updatedTool) {
-        return new ToolContract.replaceOne({ name: toolName }, updatedTool)
-    }
-
-    return {
-        'getTools': getTools,
-        'getTool': getTool,
-        'addTool': addTool
-    }
-}
+  return {
+    getTools: getTools,
+    getTool: getTool,
+    addTool: addTool,
+  };
+};
--- server/src/util/validator.js
+++
@@ -1,8 +0,0 @@
-exports.isValid = (input, schema) => {
-    const Validator = require('jsonschema').Validator;
-    const validator = new Validator();
-    const instance = input
-    const librarySchema = schema
-    const res = validator.validate(instance, librarySchema)
-    return res.valid
-}
