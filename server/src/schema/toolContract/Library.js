abbadc2ef9a64b326fcea679a48b941c024af859
mig07 <miguelfluis7@gmail.com>
2022-06-27 16:10:44 +0100

    Server: added input validation to tool contract schema

--- server/src/schema/toolContract/Library.js
+++ server/src/schema/toolContract/Library.js
@@ -1,30 +1,52 @@
-const { Integer } = require('json-schema');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const { Integer } = require("json-schema");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const CommandSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    description: { type: String },
-    invocation: [String],
-    allowedValues: [String],
-    allowedCommands: [String],
-    allowedCommandSets: [String]
-})
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 20,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    minlength: 10,
+    maxlength: 100,
+  },
+  invocation: [String],
+  allowedValues: [String],
+  allowedCommands: [String],
+  allowedCommandSets: [String],
+});
 
 const CommandGroupSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    description: { type: String },
-    invocation: [String],
-    order: { type: Number, required: true, unique: true },
-    allowedCommandRepetition: { type: Boolean, required: true },
-    commands: [CommandSchema]
-})
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    required: true,
+    unique: true,
+  },
+  description: { type: String, minlength: 10, maxlength: 100 },
+  invocation: [String],
+  order: { type: Number, min: 0, max: 10, required: true, unique: true },
+  allowedCommandRepetition: { type: Boolean, required: true },
+  commands: [CommandSchema],
+});
 
 const LibrarySchema = new Schema({
-    machineAddress: { type: String, required: true },
-    image: { type: String, unique: true },
-    name: { type: String, required: true, unique: true },
-    commandGroups: [CommandGroupSchema]
-})
+  machineAddress: { type: String, minlength: 3, maxlength: 30, required: true },
+  image: { type: String, minlength: 3, maxlength: 20, unique: true },
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    required: true,
+    unique: true,
+  },
+  commandGroups: [CommandGroupSchema],
+});
 
-module.exports = LibrarySchema
+module.exports = LibrarySchema;
--- server/src/schema/toolContract/ToolContract.js
+++ server/src/schema/toolContract/ToolContract.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const ApiSchema = require('./Api')
-const LibrarySchema = require('./Library')
+const ApiSchema = require("./Api");
+const LibrarySchema = require("./Library");
 
 const ToolContractSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-    api: ApiSchema,
-    library: LibrarySchema
-})
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 20,
+    required: true,
+    unique: true,
+  },
+  description: { type: String, minlength: 10, maxlength: 100, required: true },
+  api: ApiSchema,
+  library: LibrarySchema,
+});
 
-module.exports = mongoose.model('ToolContract',ToolContractSchema)
+module.exports = mongoose.model("ToolContract", ToolContractSchema);
