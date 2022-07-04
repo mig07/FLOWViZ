5fa6d713574c9bf70f5e84b2d02749cbac265e3f
mig07 <miguelfluis7@gmail.com>
2022-07-04 17:53:40 +0100

    Server: changed workflow schema

--- server/src/schema/workflow/workflow.js
+++ server/src/schema/workflow/workflow.js
@@ -3,29 +3,29 @@ const workflowStep = {
     exists: { errorMessage: "id is required." },
     isString: { errorMessage: "id must be a String." },
     isLength: {
-      errorMessage: "id must be at least 4 characters long.",
-      options: { min: 4, max: 20 },
+      errorMessage: "id must be at least 2 characters long.",
+      options: { min: 2, max: 20 },
     },
   },
   action: {
     exists: { errorMessage: "Action is required." },
   },
-  previousActions: {
+  parents: {
     isArray: {
       bail: true,
       options: {
         min: 0,
       },
-      errorMessage: "Previous Actions must be an array.",
+      errorMessage: "Parents must be an array.",
     },
   },
-  nextActions: {
+  children: {
     isArray: {
       bail: true,
       options: {
         min: 0,
       },
-      errorMessage: "Next Actions must be an array.",
+      errorMessage: "Children must be an array.",
     },
   },
 };
