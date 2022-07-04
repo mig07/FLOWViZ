29f2f7f8fe5ba0f19f5d08b910b1c5db26ef6518
mig07 <miguelfluis7@gmail.com>
2022-07-04 16:37:59 +0100

    Server: retrieving execution order from workflow and converting for Airflow syntax

--- server/src/service/workflowService.js
+++ server/src/service/workflowService.js
@@ -8,7 +8,44 @@ module.exports = (WorkflowDb, ApiException) => {
   }
 
   async function postWorkflow(workflow) {
+    const executionOrder = getExecutionOrder(workflow);
     return await WorkflowDb.postWorkflow(workflow);
+  }
+
+  function getExecutionOrder(workflow) {
+    let res = [];
+    workflow.forEach((step) => {
+      const children = step.children;
+      const parents = step.parents;
+      const startNode = !parents || parents.length === 0;
+      const endNode = !children || children.length === 0;
+
+      if (endNode) {
+        return;
+      } else if (startNode) {
+        res.push(step.id);
+      }
+
+      if (!res.includes(children)) {
+        res.push(children.length === 1 ? children[0] : children);
+      }
+    });
+
+    return stringifyExecutionOrder(res);
+  }
+
+  function stringifyExecutionOrder(execOrder) {
+    let res = "";
+    const lastElemIdx = execOrder.length - 1;
+    execOrder.forEach((step, index, execOrder) => {
+      if (index === lastElemIdx) return;
+      res += Array.isArray(step) ? `[${step}]` : step;
+      if (index < lastElemIdx.length - 1) {
+        res += " >> ";
+      }
+    });
+
+    return res;
   }
 
   return {
