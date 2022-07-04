6e250a4c1a3e10ddd33f8d2520903cc94ade3dc3
mig07 <miguelfluis7@gmail.com>
2022-07-04 17:53:56 +0100

    Server: changed workflow validation middleware

--- server/src/datasource/workflowDbDataSource.js
+++ server/src/datasource/workflowDbDataSource.js
@@ -9,28 +9,7 @@ module.exports = (rabbitmq) => {
     return Workflow.findOne({ id: id });
   }
 
-  function postWorkflow(workflow) {
-    rabbitmq.connect("amqp://localhost", (err0, conn) => {
-      if (err0) {
-        throw err0;
-      }
-
-      conn.createChannel((err1, channel) => {
-        if (err1) {
-          throw err1;
-        }
-
-        var queue = "workflowRequests";
-        var msg = workflow;
-
-        channel.assertQueue(queue, {
-          durable: false,
-        });
-
-        channel.sendToQueue(queue, Buffer.from(msg));
-      });
-    });
-  }
+  function postWorkflow(workflow) {}
 
   return {
     getWorkflows: getWorkflows,
--- server/src/middleware/workflowValidationMiddleware.js
+++ server/src/middleware/workflowValidationMiddleware.js
@@ -2,9 +2,13 @@ const { validationResult } = require("express-validator");
 const ApiException = require("../exception/apiException");
 
 module.exports = (req, res, next) => {
-  const errors = validationResult(req);
+  const errors = [];
+  workflow = req.body;
+  workflow.forEach((step) => {
+    errors.push(validationResult(step).array());
+  });
 
-  if (!errors.isEmpty()) {
+  if (!errors || errors.length <= 0) {
     const errorMsgs = errors.array().map((err) => err.msg);
     next(ApiException.badRequest(errorMsgs));
     return;
