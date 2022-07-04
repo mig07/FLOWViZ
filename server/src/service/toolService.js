e7801cfd6569bc4a7716f41d5af1dac4bf66d6ea
mig07 <miguelfluis7@gmail.com>
2022-07-04 17:28:43 +0100

    Server: Added documentation in services

--- server/src/service/toolService.js
+++ server/src/service/toolService.js
@@ -2,14 +2,27 @@ const ToolContract = require("../schema/toolContract/ToolContract");
 const getOne = require("./serviceUtils");
 
 module.exports = (ToolDb, ApiException) => {
+  /**
+   * Returns a list of tools from the data source
+   * @returns {A list of tools from MongoDB}
+   */
   async function getTools() {
     return await ToolDb.getTools();
   }
 
+  /**
+   * Returns a specified tool from the data source
+   * @param {The specified tool's name} toolName
+   * @returns {A specified tool from the data source from MongoDB}
+   */
   async function getTool(toolName) {
     return getOne(ToolDb.getTool, toolName, "tool");
   }
 
+  /**
+   * Sends a new tool to MongoDB
+   * @param {The tool JSON structure} tool
+   */
   async function addTool(tool) {
     const contract = new ToolContract({
       name: tool.name,
--- server/src/service/workflowService.js
+++ server/src/service/workflowService.js
@@ -1,52 +1,32 @@
+const getOne = require("./serviceUtils");
+
 module.exports = (WorkflowDb, ApiException) => {
+  /**
+   * Returns a list of workflows from the data source
+   * @returns {A list of workflows from the data source}
+   */
   async function getWorkflows() {
     return await WorkflowDb.getWorkflows();
   }
 
+  /**
+   * Returns a specified workflow from the data source
+   * @param {The workflow ID} id
+   * @returns {The specified workflow}
+   */
   async function getWorkflow(id) {
     return getOne(WorkflowDb.getWorkflow, id, "workflow");
   }
 
+  /**
+   * Sends a workflow to Apache Airflow, while formatting parameters for a correct
+   * workflow system execution
+   * @param {The workflow JSON structure} id
+   */
   async function postWorkflow(workflow) {
     const executionOrder = getExecutionOrder(workflow);
     console.log(executionOrder);
     return await WorkflowDb.postWorkflow(workflow);
-  }
-
-  function getExecutionOrder(workflow) {
-    let res = [];
-    workflow.forEach((step) => {
-      const children = step.children;
-      const parents = step.parents;
-      const startNode = !parents || parents.length === 0;
-      const endNode = !children || children.length === 0;
-
-      if (endNode) {
-        return;
-      } else if (startNode) {
-        res.push(step.id);
-      }
-
-      if (!res.includes(children)) {
-        res.push(children.length === 1 ? children[0] : children);
-      }
-    });
-
-    return stringifyExecutionOrder(res);
-  }
-
-  function stringifyExecutionOrder(execOrder) {
-    let res = "";
-    const lastElemIdx = execOrder.length - 1;
-    execOrder.forEach((step, index, execOrder) => {
-      if (index === lastElemIdx) return;
-      res += Array.isArray(step) ? `[${step}]` : step;
-      if (index < lastElemIdx - 1) {
-        res += " >> ";
-      }
-    });
-
-    return res;
   }
 
   return {
@@ -55,3 +35,51 @@ module.exports = (WorkflowDb, ApiException) => {
     postWorkflow: postWorkflow,
   };
 };
+
+/**
+ * Gets the execution order, already formatted for the Airflow syntax
+ * @param {The workflow parameter} workflow
+ * @returns {Workflow's execution order string, formatted to request Airflow}
+ */
+function getExecutionOrder(workflow) {
+  let res = [];
+  workflow.forEach((step) => {
+    const children = step.children;
+    const parents = step.parents;
+    const startNode = !parents || parents.length === 0;
+    const endNode = !children || children.length === 0;
+
+    if (endNode) {
+      return;
+    } else if (startNode) {
+      res.push(step.id);
+    }
+
+    if (!res.includes(children)) {
+      res.push(children.length === 1 ? children[0] : children);
+    }
+  });
+
+  return stringifyExecutionOrder(res);
+}
+
+/**
+ * Gets a stringified version of the passed array, already formatted with
+ * the Airflow syntax
+ * @param {The ordered array representing the workflow's execution order} execOrder
+ * @returns {A stringified version of the passed array, already formatted with
+ * the Airflow syntax}
+ */
+function stringifyExecutionOrder(execOrder) {
+  let res = "";
+  const lastElemIdx = execOrder.length - 1;
+  execOrder.forEach((step, index, execOrder) => {
+    if (index === lastElemIdx) return;
+    res += Array.isArray(step) ? `[${step}]` : step;
+    if (index < lastElemIdx - 1) {
+      res += " >> ";
+    }
+  });
+
+  return res;
+}
