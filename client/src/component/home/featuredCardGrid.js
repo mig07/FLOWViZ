b7b3b2acd32ee038811eb5158ca7373ccfd01b4a
mig07 <miguelfluis7@gmail.com>
2022-08-07 01:04:50 +0100

    Client: Updated home grid for better css breakpoint support

--- client/src/component/home/featuredCardGrid.js
+++ client/src/component/home/featuredCardGrid.js
@@ -1,37 +1,37 @@
-import * as React from 'react';
-import { Grid } from '@material-ui/core';
-import { Box } from '@material-ui/core';
-import FeaturedCard from './featuredCard';
+import * as React from "react";
+import { Grid } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+import FeaturedCard from "./featuredCard";
 
 export default function FeaturedCardGrid() {
-    return (
-        <Box sx={{mt: 2}}>
-            <Grid container spacing={5}>
-                <Grid item xs={4}>
-                    <FeaturedCard 
-                        title="Templated workflows" 
-                        description="Use the available provided tools to build workflows."
-                        buttonText="Learn more"
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <FeaturedCard 
-                        title="Custom workflows" 
-                        description="Provide new tools to build 
+  return (
+    <Box sx={{ mt: 2 }}>
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={4}>
+          <FeaturedCard
+            title="Templated workflows"
+            description="Use the available provided tools to build workflows."
+            buttonText="Learn more"
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <FeaturedCard
+            title="Custom workflows"
+            description="Provide new tools to build 
                         your custom workflow, along with the available ones."
-                        buttonText="Learn more"
-                        buttonUrl="/whiteboard"
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <FeaturedCard 
-                        title="What we have" 
-                        description="Check the documentation to see the available tools."
-                        buttonText="Learn more"
-                        buttonUrl="/documentation"
-                    />
-                </Grid>
-            </Grid>                    
-        </Box>
-    )
+            buttonText="Learn more"
+            buttonUrl="/whiteboard"
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <FeaturedCard
+            title="What we have"
+            description="Check the documentation to see the available tools."
+            buttonText="Learn more"
+            buttonUrl="/documentation"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
