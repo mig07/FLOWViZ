e9d17e1dda6b3a7ea9ff93aa95d543062062f48d
mig07 <miguelfluis7@gmail.com>
2022-08-07 01:03:33 +0100

    Client: Updated documentation page

--- client/src/page/documentation.js
+++ client/src/page/documentation.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Toolbar } from "@mui/material";
-import Apis from "../component/documentation/apis";
-import Libraries from "../component/documentation/libraries";
+import Typography from "@mui/material/Typography";
+import Loading from "../component/common/loading";
+import GenericError from "../component/common/genericError";
+import ToolCardGrid from "../component/documentation/toolCardGrid";
+import ToolService from "../service/toolService";
+import PageTitle from "../component/common/pageTitle";
 
 export default function Documentation({ config }) {
+  const toolService = new ToolService(config);
+
   return (
     <>
       <Toolbar />
-      <Libraries config={config} />
-      <Apis config={config} />
+      <Typography variant="h3" align="center">
+        Available tools
+        {toolService.getTools(GenericError, ToolCardGrid, <Loading />)}
+      </Typography>
     </>
   );
 }
