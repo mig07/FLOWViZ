5673732a7361025f4aed296d335248c294b24c7b
mig07 <miguelfluis7@gmail.com>
2022-08-11 01:25:09 +0100

    Server: updated DockerOperator mapper

--- server/src/mappers/mapTaskToDockerOperator.js
+++ server/src/mappers/mapTaskToDockerOperator.js
@@ -1,18 +1,20 @@
 const DockerOperatorModule = require("../schema/airflow/DockerOperator");
+const Mounts = require("../schema/airflow/Mounts");
 const DockerOperator = DockerOperatorModule.DockerOperator;
 const DockerOperatorParams = DockerOperatorModule.DockerOperatorParams;
 
 function mapToTaskToDockerOperator(task) {
+  const action = task.action;
   return new DockerOperator(
     (id = task.id),
     (operator_params = new DockerOperatorParams(
-      (image = task.action.image),
-      (api_version = task.action.api_version),
-      (mounts = task.action.mounts),
-      (command = task.action.command),
-      (auto_remove = task.action.auto_remove),
-      (docker_url = task.action.docker_url),
-      (network_mode = task.action.network_mode)
+      (image = action.image),
+      (api_version = action.api_version),
+      (mounts = action.mounts),
+      (command = action.command),
+      (auto_remove = action.auto_remove),
+      (docker_url = action.docker_url),
+      (network_mode = action.network_mode)
     ).toJson())
   ).toJson();
 }
