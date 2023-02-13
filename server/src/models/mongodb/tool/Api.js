efde861de1ef2a3a0a45c897d7a42635d93366f0
mig07 <miguelfluis7@gmail.com>
2023-02-13 23:02:34 +0000

    Server: fixed API tool domain model

--- server/src/models/mongodb/tool/Api.js
+++ server/src/models/mongodb/tool/Api.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const EndpointSchema = new Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String },
     path: { type: String, required: true },
     method: { type: String, required: true },
     headers: { type: Object, required: true },
@@ -13,13 +13,4 @@ const EndpointSchema = new Schema(
   { _id: false }
 );
 
-const ApiSchema = new Schema(
-  {
-    url: { type: String, required: true },
-    apiKey: { type: String, required: true },
-    endpoints: [EndpointSchema],
-  },
-  { _id: false }
-);
-
-module.exports = ApiSchema;
+module.exports = EndpointSchema;
--- server/src/models/mongodb/tool/ToolContract.js
+++ server/src/models/mongodb/tool/ToolContract.js
@@ -3,14 +3,14 @@ const Schema = mongoose.Schema;
 
 const General = require("./General");
 const Access = require("./Access");
-const ApiSchema = require("./Api");
+const EndpointSchema = require("./Api");
 const CommandGroupSchema = require("./Library");
 
 const ToolContractSchema = new Schema({
   general: General,
   access: Access,
   library: [CommandGroupSchema],
-  api: [ApiSchema],
+  api: [EndpointSchema],
 });
 
 module.exports = mongoose.model("ToolContract", ToolContractSchema);
