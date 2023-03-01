f3ed79fb20bfac4e687da04678aa4df44544744b
mig07 <miguelfluis7@gmail.com>
2023-03-01 20:03:01 +0000

    Added setup shell script to automatically setup project

---
+++ setup.sh
@@ -0,0 +1,53 @@
+# If docker daemon is off, turns it on.
+startDocker() {
+    if [ $(systemctl is-active docker) != "active" ];
+    then
+        sudo systemctl start docker.service containerd.service
+        echo Starting docker...
+    else
+        echo Docker is active!
+    fi
+}
+
+# Downloads MongoDB's image and creates the container exposing the 27017 port.
+downloadAndCreateMongoContainer() {
+    docker pull mongo
+    docker run --name mongodb -d -p 27017:27017 mongo
+}
+
+# Creates Airflow user and composes services.
+setupAirflow() {
+    cd airflow/ && echo -e "AIRFLOW_UID=$(id -u)" > .env && docker compose up
+}
+
+# Creates a Docker network and adds the Airflow and MongoDB containers.
+setupDockerNetwork() {
+    docker network create flowviz-docker-network
+
+    docker network connect flowviz-docker-network mongodb && \
+    for airflowContainer in $(docker ps --format {{.Names}} | grep "airflow-"); \
+    do \
+        docker network connect flowviz-docker-network $airflowContainer; \
+    done
+
+    docker inspect -f '{{with index .NetworkSettings.Networks "flowviz-network-docker"}}{{.IPAddress}}{{end}}' mongodb
+}
+
+setupFlowviz() {
+    npm run setup
+
+    # Copying a default .env files, if user-defined .env files do not exist in server and client folders.
+    if [ ! -f ./.env ]; then
+        cp .defaults/server/.env .
+    fi
+
+    if [ ! -f ./client/.env ]; then
+        cp .defaults/client/.env client/
+    fi
+}
+
+startDocker
+downloadAndCreateMongoContainer
+setupAirflow
+setupDockerNetwork
+setupFlowviz
